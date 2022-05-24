c252e10b2197c2d2edb46476e4a6597b37ee2d9e
Leonardo130709 <78939351+Leonardo130709@users.noreply.github.com>
2022-05-24 14:12:28 +0300

    added discrete wrapper

---
+++ wrappers/discretize.py
@@ -0,0 +1,21 @@
+from .base import Wrapper
+from dm_env import specs
+import numpy as np
+
+
+class DiscreteActionWrapper(Wrapper):
+    def __init__(self, env, num_actions: int, one_hot=True):
+        super().__init__(env)
+        act_spec = env.action_spec()
+        self.one_hot = one_hot
+        self._action_spec = specs.BoundedArray(shape=act_spec.shape, dtype=int, minimum=0, maximum=num_actions - 1)
+        self._spacing = np.linspace(act_spec.minimum, act_spec.maximum, num_actions)
+
+    def step(self, action):
+        if self.one_hot:
+            action = action.argmax(-1)
+        action = np.take_along_axis(self._spacing, action, 0)
+        return self.env.step(action)
+
+    def action_spec(self):
+        return self._action_spec
