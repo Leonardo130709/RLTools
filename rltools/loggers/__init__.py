e85efdc359cbd6cf6cf12252defe91154d1fce66
Leonardo130709 <78939351+Leonardo130709@users.noreply.github.com>
2022-09-10 00:07:16 +0300

    terminal logger

--- rltools/loggers/__init__.py
+++ rltools/loggers/__init__.py
@@ -1,4 +1,4 @@
 """Loggers inspired by ACME(github.com/deepmind/acme)."""
 from .jsonl import JSONLogger
 from .tf_summary import TFSummaryLogger
-from .terminal import TerminalLogger
+from .terminal import TerminalOutput
